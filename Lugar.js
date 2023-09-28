fbc1bc159054159550f6b24a4909fa7f03c73659
rajeshkannan2003 <rajeshkannan8734@gmail.com>
2023-09-28 14:35:23 +0530

    LUGAR TASK

--- Lugar.js
+++ Lugar.js
@@ -1,6 +1,6 @@
 function animateNumber(targetId, initialValue, finalValue, duration) {
     const element = document.getElementById(targetId);
-    const increment = (finalValue - initialValue) / (duration / 16); // 16ms is roughly 1 frame at 60fps
+    const increment = (finalValue - initialValue) / (duration / 16); 
 
     let currentValue = initialValue;
 
@@ -17,6 +17,6 @@ function animateNumber(targetId, initialValue, finalValue, duration) {
 }
 
 // Call the animation function for each element
-animateNumber("previous-projects", 1, 34 , 2000  ); // 2000ms (2 seconds) duration
+animateNumber("previous-projects", 1, 34 , 2000  ); 
 animateNumber("years-experience", 1, 20, 2000);
 animateNumber("ongoing-projects", 1, 12, 2000);
